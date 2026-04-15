73c3fd5344006adbc12661159f70648d022727fc
li.hao85 <lihao_19850403@126.com>
2026-04-15 16:44:46 +0800

    Use ActivityResultLauncher instead of startActivityForResult.

--- app/src/main/java/com/lihao/kotlinapp/pages/contact/ContactPickerActivity.kt
+++ app/src/main/java/com/lihao/kotlinapp/pages/contact/ContactPickerActivity.kt
@@ -8,12 +8,40 @@ import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.lihao.kotlinapp.R
 
-const val PICK_CONTACT_REQUEST_CODE = 0
 const val PERMISSION_REQUEST_CODE = 0x133
 class ContactPickerActivity: AppCompatActivity() {
+
+    /** 使用新的ActivityResultLauncher代替startActivityForResult。 */
+    private val resultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode != RESULT_OK || result.data == null || result.data?.data == null) {
+            return@registerForActivityResult
+        }
+        val contactData: Uri? = result.data?.data
+        if (contactData == null) {
+            return@registerForActivityResult
+        }
+        val resolver = contentResolver
+        val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER)
+        val cursor = resolver.query(contactData, projection, null, null, null)
+        if (cursor != null && cursor.moveToNext()) {
+            val nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+            val name = cursor.getString(nameIndex)
+            val phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            val phone = cursor.getString(phoneIndex)
+            val showView: TextView = findViewById(R.id.show)
+            showView.text = name
+            val phoneView: TextView = findViewById(R.id.phone)
+            phoneView.text = phone
+        }
+        cursor?.close()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,36 +63,7 @@ class ContactPickerActivity: AppCompatActivity() {
                 val intent = Intent()
                 intent.action = Intent.ACTION_PICK
                 intent.type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
-                startActivityForResult(intent, PICK_CONTACT_REQUEST_CODE)
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != RESULT_OK || data == null) {
-            return
-        }
-        when (requestCode) {
-            PICK_CONTACT_REQUEST_CODE -> {
-                val contactData: Uri? = data.data
-                if (contactData == null) {
-                    return
-                }
-                val resolver = contentResolver
-                val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER)
-                val cursor = resolver.query(contactData, projection, null, null, null)
-                if (cursor != null && cursor.moveToNext()) {
-                    val nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-                    val name = cursor.getString(nameIndex)
-                    val phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
-                    val phone = cursor.getString(phoneIndex)
-                    val showView: TextView = findViewById(R.id.show)
-                    showView.text = name
-                    val phoneView: TextView = findViewById(R.id.phone)
-                    phoneView.text = phone
-                }
-                cursor?.close()
+                resultLauncher.launch(intent)
             }
         }
     }
